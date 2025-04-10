6ffac59e4b37221ee1f75bd995c6631f7dc578d1
Magaca78 <mateo.1701920666@ucaldas.edu.co>
2025-04-10 15:29:20 -0500

    Primer commit

---
+++ cemeteryProject-management/src/main/java/com/cementerio/cemeteryProject_management/CemeteryProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cementerio.cemeteryProject_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemeteryProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemeteryProjectManagementApplication.class, args);
+	}
+
+}
---
+++ cemeteryProject-management/src/test/java/com/cementerio/cemeteryProject_management/CemeteryProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cementerio.cemeteryProject_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemeteryProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
